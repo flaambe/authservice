4cdeb181687127dbb090b75743a0ad586feb104d
Grigoriy <flaambe@gmail.com>
2020-07-18 23:15:06 +0500

    views refactor

--- views/auth.go
+++ views/auth.go
@@ -1,5 +1,17 @@
 package views
 
+type AuthRequest struct {
+	GUID string `json:"guid"`
+}
+
+type RefreshTokenRequest struct {
+	RefreshToken string `json:"refresh_token"`
+}
+
+type DeleteTokenRequest struct {
+	RefreshToken string `json:"refresh_token"`
+}
+
 type AuthResponse struct {
 	AccessToken  string `json:"access_token"`
 	TokenType    string `json:"token_type"`
@@ -12,8 +24,4 @@ type RefreshResponse struct {
 	TokenType    string `json:"token_type"`
 	ExpiresIn    int    `json:"expires_in"`
 	RefreshToken string `json:"refresh_token"`
-}
-
-type ErrorResponse struct {
-	ErrorMessage string `json:"error_message"`
 }
---
+++ views/error.go
@@ -0,0 +1,5 @@
+package views
+
+type ErrorResponse struct {
+	ErrorMessage string `json:"error_message"`
+}
